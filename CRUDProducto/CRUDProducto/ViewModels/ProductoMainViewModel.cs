dfd15a6dfac02de29fd90c8c0ba7786022768869
Rodolfo Chacon <rudychacon01@gmail.com>
2024-09-20 15:34:54 -0600

    Create ProductoMainViewModel.cs

---
+++ CRUDProducto/CRUDProducto/ViewModels/ProductoMainViewModel.cs
@@ -0,0 +1,22 @@
+﻿
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CRUDProducto.Models;
+using CRUDProducto.Services;
+using System.Collections.ObjectModel;
+
+namespace CRUDProducto.ViewModels
+{
+    public partial class ProductoMainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<Producto> productoCollection = new ObservableCollection<Producto>();
+
+        private readonly ProductoService productoService;
+
+        public ProductoMainViewModel()
+        {
+            productoService = new ProductoService();
+        }
+    }
+}
