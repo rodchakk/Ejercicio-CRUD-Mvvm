e0b51eb9ccf21b23bcd706ae6812d4f4f0454a95
Rodolfo Chacon <rudychacon01@gmail.com>
2024-09-20 15:29:47 -0600

    empezanod proyecto

---
+++ Crud Productos/Crud Productos/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Crud_Productos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Crud Productos/Crud Productos/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Crud_Productos
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
